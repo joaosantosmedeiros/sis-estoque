a134ef43599abe26737750ad6ae927d15ea2cdcc
joaosantosmedeiros <jopesame@gmail.com>
2025-05-12 20:03:32 -0300

    add message dialog confirm

---
+++ estoque-front/src/app/shared/components/message-dialog-confirm/message-dialog-confirm.component.ts
@@ -0,0 +1,24 @@
+import { Component, Inject, Optional } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-message-dialog-confirm',
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatDialogClose,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogTitle
+  ],
+  templateUrl: './message-dialog-confirm.component.html',
+  styleUrl: './message-dialog-confirm.component.scss'
+})
+export class MessageDialogConfirmComponent {
+
+  constructor(@Optional() @Inject(MAT_DIALOG_DATA) public msg: string) { }
+
+}
